59938ed17150647775279e509c931eddce7f6ca4
ayyoubhalbaoui1 <ayyoubhalbaoui1@gmail.com>
2021-01-27 14:29:10 +0100

    Part 1

---
+++ Covid-App/src/index.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) {
+    app.quit();
+}
+
+const createWindow = () => {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 1200,
+        webPreferences: {
+            nodeIntegration: true,
+        }
+    });
+
+    // and load the index.html of the app.
+    mainWindow.loadFile(path.join(__dirname, '/views/dashboard.html'));
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+};
+
+
+app.on('ready', createWindow);
+
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
