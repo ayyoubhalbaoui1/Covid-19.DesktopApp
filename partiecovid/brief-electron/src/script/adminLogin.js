8f6ac984e4dd2aa1f64605c88fce99a633c7bed4
Idoufkir <mustafa.idoufkir@gmail.com>
2021-01-29 13:53:58 +0100

    Update

---
+++ partiecovid/brief-electron/src/script/adminLogin.js
@@ -0,0 +1,40 @@
+function checkLogin() {
+    let login = document.getElementById("login").value;
+    let loginErr = document.getElementById("loginErr");
+    if (login === "") {
+        loginErr.innerHTML = "please fill out this field ";
+    } else {
+        loginErr.innerHTML = " ";
+    }
+}
+
+function checkPassword() {
+    let password = document.getElementById("password").value;
+    let passwdErr = document.getElementById("passwdErr");
+
+    if (password === "") {
+        passwdErr.innerHTML = "please fill out this field ";
+    } else {
+        passwdErr.innerHTML = " ";
+    }
+}
+
+function check() {
+    let login = document.getElementById("login").value;
+    let loginErr = document.getElementById("loginErr");
+    let password = document.getElementById("password").value;
+    let passwdErr = document.getElementById("passwdErr");
+
+    let user_name = "admin";
+    let user_password = "admin";
+    
+    if (login !== user_name) {
+        loginErr.innerHTML = "please fill out this field ";
+    } else if (password !== user_password) {
+        passwdErr.innerHTML = "please fill out this field ";
+    } else {
+        loginErr.innerHTML = " ";
+        passwdErr.innerHTML = " ";
+        window.location.href = '../views/dashboard.html';
+    }
+}
